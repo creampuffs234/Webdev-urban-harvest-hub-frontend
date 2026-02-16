9e05d5d9eb893e170666e84418f5ea5333f51884
creampuffs234 <kenajason1799@example.com>
2026-02-17 03:01:51 +0530

    chore: Finalize dark mode and backend cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,15 @@ export default {
   theme: {
     extend: {
       colors: {
-        // Main Brand Colors
+        
         "chelsea-cucumber": "#4e381b",
         "atlantis": "#ffe600",
         "oreyelo": "#6db655",
 
-        // Dark Mode Specific
-        "dark-bg": "#1d2c19",      // Your dark background
-        "dark-secondary": "#34613e", // Renamed from 'whitetext' (it is green)
-        "dark-tertiary": "#ac810a",  // Renamed from 'thurdcol'
+        
+            "dark-bg": "#1d2c19",      
+            "dark-secondary": "#34613e", 
+            "dark-tertiary": "#ac810a",  
       },
       fontFamily: {
         helvetica: ["Helvetica", "sans-serif"],
